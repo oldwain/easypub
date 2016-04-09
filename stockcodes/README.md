ff9d1b1d71d076040073fb5dfc9f282b7a7cdb7a
oldwain <oldwain@163.com>
2016-04-09 15:02:31 +0800

    增加代码分类功能，设置缺省代码表

---
+++ stockcodes/README.md
@@ -0,0 +1,57 @@
+# easypub
+easyquant/easyhistory/easytrader/easyquant的公用模块
+包含的package:
+    stockcodes: 处理与股票代码表相关的功能
+    (目前仅此一个)
+
+
+### 引入
+
+```python
+import stockcodes
+```
+### 用法
+
+
+### 更新代码表及各个分类代码表
+
+```python
+stockcodes.update_stock_codes()
+```
+更新后缺省代码表重置为全部代码表，如果希望缺省代码表为某个分类代码表， 需调用set_default_scope设置缺省代码表
+
+### 获取代码表
+
+```python
+stockcodes.get_stock_codes(scope=['default'])
+```
+可以使用列表同时获取多个分类, scope=['all'] 可以获取全部代码表
+scope=['default'] 获取当前缺省代码表
+scope=['index','fund'] 获取指数及基金代码表
+
+### 设置缺省代码表
+
+```python
+stockcodes.set_default_scope(scope=['all'])
+```
+
+### 查询代码对应的品种类型
+```python
+stock_type = stockcodes.code_type(stock_code)
+```
+返回值：
+index, fund, bond, cb(可转债), eb(可交换债), repo(逆回购), fundnav(基金净值),bstock, astock, other
+
+注：
+
+目前使用的分类及对应的代码表文件有：
+scopeandfile = {'all': 'stock_codes_all.conf',
+              'index': 'stock_codes_idx.conf',
+              'fund': 'stock_codes_fund.conf',
+              'astock': 'stock_codes_astock.conf',
+              'bstock': 'stock_codes_bstock.conf',
+              'defulat': 'stock_codes.conf',
+              'self': 'stock_codes_self.conf'
+              }
+其中self为自选代码表，自行编辑，update_stock_codes时不做更新，但可以读取。
+
--- stockcodes/stock_codes.py
+++ stockcodes/stock_codes.py
@@ -6,7 +6,14 @@ import requests
 
 STOCK_CODE_PATH = 'stock_codes.conf'
 
-
+scopeandfile = {'all': 'stock_codes_all.conf',
+              'index': 'stock_codes_idx.conf',
+              'fund': 'stock_codes_fund.conf',
+              'astock': 'stock_codes_astock.conf',
+              'bstock': 'stock_codes_bstock.conf',
+              'defulat': 'stock_codes.conf',
+              'self': 'stock_codes_self.conf'
+              }
 def update_stock_codes():
     """获取所有股票 ID 到 all_stock_code 目录下"""
     all_stock_codes_url = 'http://www.shdjt.com/js/lib/astock.js'
@@ -15,22 +22,78 @@ def update_stock_codes():
     stock_codes0 = grep_stock_codes.findall(response.text)
 
     stock_codes = list(
-        map(lambda stock_code: ('%s' if stock_code.startswith('s') else(
+        map(lambda stock_code: ('%s' if stock_code.startswith(('s','o')) else(
         'sh%s' if stock_code.startswith(('5', '6', '9'))  else 'sz%s')) % stock_code,
             stock_codes0))
 
-    with open(stock_code_path(), 'w') as f:
+    for sf in scopeandfile :
+        if sf != 'self':
+            with open(stock_code_path(sf), 'w') as f:
+                scope_codes = [x for x in stock_codes if code_type(x) == sf]
+                f.write(json.dumps(dict(stock=scope_codes)))
+
+    with open(stock_code_path('all'), 'w') as f:
         f.write(json.dumps(dict(stock=stock_codes)))
-    return stock_codes
 
-def get_stock_codes(realtime=False):
+
+    with open(stock_code_path('default'), 'w') as f:
+        f.write(json.dumps(dict(stock=stock_codes)))
+
+
+def set_default_scope(scope ):
+    stock_codes = get_stock_codes(scope)
+    with open(stock_code_path('default'), 'w') as f:
+        f.write(json.dumps(dict(stock=stock_codes)))
+
+
+
+def get_stock_codes(scope = ['default'], refresh=False):
     """获取所有股票 ID 到 all_stock_code 目录下"""
-    if realtime:
-        return update_stock_codes()
+    if refresh:
+        update_stock_codes()
+
+    if 'all' in scope:
+
+        with open(stock_code_path('all')) as f:
+                return json.load(f)['stock']
+
+
+    result = []
+    for s in scope:
+        with open(stock_code_path(s)) as f:
+            data = json.load(f)['stock']
+        result = result + data
+    return result
+
+def stock_code_path(scope='default'):
+    if scope == 'default':
+        return os.path.join(os.path.dirname(__file__), STOCK_CODE_PATH)
     else:
-        with open(stock_code_path()) as f:
-            return json.load(f)['stock']
+        return os.path.join(os.path.dirname(__file__), scopeandfile[scope] )
 
 
-def stock_code_path():
-    return os.path.join(os.path.dirname(__file__), STOCK_CODE_PATH)
+def code_type(stock_code):
+    if stock_code.startswith(('sh00', 'sz39')):
+        return 'index'
+    elif stock_code.startswith(('sh5','sz15','sz16','sz18')) :
+        return 'fund' #基金，包含封基、etf、lof等
+    elif stock_code.startswith(('sh01','sh02', 'sh10','sh12','sh13', 'sz10','sz11')):
+        return 'bond' #债券
+    elif stock_code.startswith(('sh11', 'sz12')):
+        return 'cb' #可转债
+    elif stock_code.startswith(('sh132', 'sz12')):
+        return 'eb' #可交换债
+    elif stock_code.startswith(('sh20','sz13')):
+        return 'repo'  #逆回购
+    elif stock_code.startswith('o'):
+        return 'fundnav'
+    elif stock_code.startswith(('sh9','sz2')):
+        return 'bstock'
+    elif stock_code.startswith(('sh6', 'sz0','sz3')):
+        return 'astock'
+    else:
+        return 'other'
+
+
+#if __name__ == 'main':
+update_stock_codes()
